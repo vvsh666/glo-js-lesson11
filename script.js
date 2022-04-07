72ad7b983f7f4cc904f696691165a1ee1c979594
svv <vvsh1978@mail.ru>
2022-04-07 19:24:40 +0500

    feat: make lesson11

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+const btn = document.getElementById('btn');
+const inputText = document.getElementById('text');
+const square = document.getElementById('square');
+const eBtn = document.getElementById('e_btn');
+const inputRange = document.getElementById('range');
+const circle = document.getElementById('circle');
 
+const changeColor = function () {
+    square.style.backgroundColor = inputText.value;
+}
 
+const changeDim = function () {
+    circle.style.width = inputRange.value + '%'
+    circle.style.height = inputRange.value + '%'
+}
 
+btn.addEventListener('click', changeColor);
 
+eBtn.style.display = 'none';
 
+inputRange.addEventListener('input', changeDim)
