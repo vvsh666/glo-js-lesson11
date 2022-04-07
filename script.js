2323daf5e19ea041fcea8b2436076b2041e362d9
svv <vvsh1978@mail.ru>
2022-04-07 22:01:38 +0500

    fix: set start inputRange & change range value into span

--- script.js
+++ script.js
@@ -4,18 +4,29 @@ const square = document.getElementById('square');
 const eBtn = document.getElementById('e_btn');
 const inputRange = document.getElementById('range');
 const circle = document.getElementById('circle');
+const rangeSpan = document.getElementById('range-span')
 
 const changeColor = function () {
     square.style.backgroundColor = inputText.value;
 }
 
 const changeDim = function () {
+    rangeSpan.textContent = inputRange.value + '%'
     circle.style.width = inputRange.value + '%'
     circle.style.height = inputRange.value + '%'
+}
+
+const setRange = function () {
+    let startPercent = 100 * parseInt(getComputedStyle(circle).height) / parseInt(getComputedStyle(square).height);
+    inputRange.value = startPercent;
+    rangeSpan.textContent = inputRange.value + '%'
 }
 
 btn.addEventListener('click', changeColor);
 
 eBtn.style.display = 'none';
 
+setRange();
+
 inputRange.addEventListener('input', changeDim)
+
